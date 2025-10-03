9c9808c11f806800b3648956f1e1e6b092904082
Kai Wang <honeyland414>
2025-10-02 21:22:20 -0400

    initialization

---
+++ src/main/java/com/example/elkdemo/ElKdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.elkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElKdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElKdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/elkdemo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.elkdemo.controller;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        logger.info("logback de logger");
+
+        return "hello, ELK";
+    }
+}
