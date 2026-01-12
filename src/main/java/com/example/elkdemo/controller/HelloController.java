03dbc59e53fdffbea101c8e974deeda7d5bced4f
Kai Wang <honeyland414>
2026-01-12 17:28:05 -0500

    update

--- src/main/java/com/example/elkdemo/controller/HelloController.java
+++ src/main/java/com/example/elkdemo/controller/HelloController.java
@@ -5,6 +5,7 @@ package com.example.elkdemo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,12 @@ public class HelloController {
     static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(@RequestParam int id) throws Exception {
 
-        logger.info("logback de logger");
+        if (id == 0) {
+            throw new Exception("id is 0");
+        }
+        logger.info("logback de logger, id is: " + id);
 
         return "hello, ELK";
     }
